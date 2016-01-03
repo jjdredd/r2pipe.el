ed6e0e47c39f4fe2f9d238f6538404a15e9ed958
Judge Dredd (key 6E23685A) <the.guard@mail.ru>
2016-01-03 10:51:21 -0700

    fixed async output with -0, needs further fixing regarding global variables (see source)

--- r2pipe.el
+++ r2pipe.el
@@ -6,25 +6,37 @@
 (require 'json)
 
 ;; Temporary storage for r2 process std output
+;; FIXME: this shouldn't be a global variable
+;; make a list with "process" structure instead and return this
+;; buffer with it in a list or cons
 (setq r2-pipe-out-string nil)
 
 (defun r2-pipe-filter (process output)
   "This filter callback is used by emacs whenever a process has output"
   (setq r2-pipe-out-string (concat r2-pipe-out-string output)))
 
+(defun r2-pipe-read (process)
+  "Reads process output till \x00"
+  (setq r2-pipe-out-string nil)
+  (while (progn (accept-process-output process)
+		(not (string-suffix-p "\x00" r2-pipe-out-string))))
+  (concat r2-pipe-out-string))
+
 (defun r2-pipe-new (cmdline)
   "Spawn r2 with cmdline and return process object on success or nil on failure"
-  (let ((process (start-process "radare2" nil "r2" "-q" cmdline)))
+  ;; "-0" also imlplies quiet, scr.color = false, scr.prompt = false
+  ;; and scr.interactive = false
+  (let ((process (start-process "radare2" nil "r2" "-0" cmdline)))
     (if (equal (process-status process) 'run)
-	(progn (set-process-filter process 'r2-pipe-filter) process)
+	(progn (set-process-filter process 'r2-pipe-filter)
+	       (r2-pipe-read process) 	;skip \x00 after r2 init
+	       process)
       nil)))
 
 (defun r2-cmd (process command)
   "Executes an r2 command and returns output in a string"
-  (setq r2-pipe-out-string nil)
   (process-send-string process (format "%s\n" command))
-  (accept-process-output process .1)
-  (concat r2-pipe-out-string))
+  (r2-pipe-read process))
 
 (defun r2-cmd-json (process command)
   "Executes a json r2 command and returns output in an elisp object"
--- test.el
+++ test.el
@@ -1,6 +1,16 @@
+;;
+;; test R2PIPE in Emacs Lisp
+;; Judge Dredd (jjdredd @ github) the.guard@mail.ru
+;;
+;; run with emacs --script test.el
+;; if r2pipe.el isn't in the same directory,
+;; modify its path in the script below
+;;
+
+
 (load-file "r2pipe.el")
 
 (setq r2p (r2-pipe-new "/bin/ls"))
-(r2-cmd r2p "e scr.color = false")
-(print (r2-cmd r2p "pd 100"))
+(print (r2-cmd r2p "pd 10"))
+(print (r2-cmd-json r2p "pdj 10"))
 (r2-pipe-close r2p)
